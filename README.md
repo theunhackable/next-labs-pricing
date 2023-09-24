e6fcd01317c074f376ef9aebeb7dd7893937f559
theunhackable <srirangasaipagala@gmail.com>
2023-09-24 23:36:03 +0530

    successfully able to send data to makeetoo forms

---
+++ README.md
@@ -0,0 +1,4 @@
+## Resources: 
+
+- [bootstrap modal components](https://getbootstrap.com/docs/4.5/components/modal/#modal-components)
+- [makeetoo docs](https://forms.maakeetoo.com/docs/)
---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('button-free').addEventListener('click', function () {
+    $('#exampleModal').modal('show'); // Show the modal
+});
+
+document.getElementById('button-pro').addEventListener('click', function () {
+    $('#exampleModal').modal('show'); // Show the modal
+});
+
+document.getElementById('button-enterprise').addEventListener('click', function () {
+    $('#exampleModal').modal('show'); // Show the modal
+});
+
+
